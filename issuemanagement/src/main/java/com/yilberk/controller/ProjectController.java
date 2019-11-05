6789cdc839b6eede705f108a96fef14241c86b00
Thecengo <cengoyilmaz801@gmail.com>
2019-11-05 22:40:41 +0300

    swagger added

--- issuemanagement/src/main/java/com/yilberk/controller/ProjectController.java
+++ issuemanagement/src/main/java/com/yilberk/controller/ProjectController.java
@@ -17,9 +17,13 @@ import com.yilberk.dto.ProjectDto;
 import com.yilberk.service.ProjectService;
 import com.yilberk.util.ApiPaths;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 
 @RestController
 @RequestMapping(ApiPaths.ProjectControllerPath.path)
+@Api(value = ApiPaths.ProjectControllerPath.path,description= "project APIs")
 public class ProjectController {
 
 	private final ProjectService projectService;
@@ -30,6 +34,7 @@ public class ProjectController {
 	}
 
 	@GetMapping("/{id}")
+	@ApiOperation(value = "get by id operation", response = ProjectDto.class)
 	public ResponseEntity<ProjectDto> getById(@PathVariable(value = "id", required = true) Long id) {
 		ProjectDto projectDto = projectService.getById(id);
 		return ResponseEntity.ok(projectDto);
@@ -37,11 +42,13 @@ public class ProjectController {
 	}
 
 	@PostMapping()
+	@ApiOperation(value = "create operation", response = ProjectDto.class)
 	public ResponseEntity<ProjectDto> createProject(@Valid @RequestBody ProjectDto projectDto) {
 		return ResponseEntity.ok(projectService.save(projectDto));
 
 	}
 	@PutMapping("/update/{id}")
+	@ApiOperation(value = "update operation", response = ProjectDto.class)
 	public ResponseEntity<ProjectDto> updateProject(@PathVariable(value = "id", required = true) Long id,@Valid @RequestBody ProjectDto projectDto) {
 
 		return ResponseEntity.ok(projectService.update(id, projectDto));
@@ -49,6 +56,7 @@ public class ProjectController {
 	}
 	
 	@DeleteMapping("/{id}")
+	@ApiOperation(value = "delete operation", response = Boolean.class)
 	public ResponseEntity<Boolean> deleteProject(@PathVariable(value = "id", required = true) Long id) {
 
 		return ResponseEntity.ok(projectService.delete(id));
--- issuemanagement/src/main/java/com/yilberk/dto/ProjectDto.java
+++ issuemanagement/src/main/java/com/yilberk/dto/ProjectDto.java
@@ -2,6 +2,8 @@ package com.yilberk.dto;
 
 import javax.validation.constraints.NotNull;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,14 +11,18 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel(value = "Project data transfer object")
 public class ProjectDto {
 	
+	@ApiModelProperty(value = "Project id")
 	private Long id;
 	
 	@NotNull
+	@ApiModelProperty(required = true, value = "Name of Project")
 	private String projectName;
 	
 	@NotNull
+	@ApiModelProperty(required = true, value = "Code of Project")
 	private String projectCode;
 
 }
